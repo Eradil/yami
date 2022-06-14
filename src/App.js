2e3c321056b41aadc8b3c44b06272174411a8b06
Eradil <eradil.abasbekov.ss@gmail.comgit config --global user.name Eradil git config --global user.name Eradil git config --global user.email eradil.abasbekov.ss@gmail.com>
2022-06-14 23:03:54 +0600

    add components

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import "./scss/app.scss";
 const App = () => {
   return (
     <div>
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               <PizzaBlock title="Четыре-сыра" price={345} />
               <PizzaBlock title="Pepproni" price={123} />
               <PizzaBlock title="Четыре-пять" price={222} />
