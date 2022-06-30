c925961bbdad0760d28dee311412a39fe26a5527
Eradil <eradil.abasbekov.ss@gmail.comgit config --global user.name Eradil git config --global user.name Eradil git config --global user.email eradil.abasbekov.ss@gmail.com>
2022-06-30 23:42:40 +0600

    add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,6 +51,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? sceleton : pizzas}</div>
+      {/* pagination */}
       <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
